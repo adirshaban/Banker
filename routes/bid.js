566735780584e5ea53bb01bac90e60750181152b
Adir Shaban <adirshaban1995@gmail.com>
2020-06-13 17:08:09 +0300

    implemented controllers for bid and lose

--- routes/bid.js
+++ routes/bid.js
@@ -1,16 +1,41 @@
 const router = require("express").Router();
+const { campagins, redisClient } = require("../redis-fetch");
 
 router.post("/", (req, res) => {
-    /* body should be auction_id , campaign_id, price
-    check if price < on memory campaign budget if so return true else false
-     */
+	const { auction_id, campaign_id, price } = req.body;
+	const campaignBudget = campagins[campaign_id];
+
+	console.log("budget", campaignBudget);
+
+	if (!campaignBudget) {
+		return res.status(404).json({ message: `campaign ${campaign_id} does not exist` });
+	}
+
+	if (price <= campaignBudget) {
+		campagins[campaign_id] -= price;
+		res.json({ didBid: true });
+		redisClient.hmset(`auctions:${auction_id}`, "campaign_id", campaign_id, "price", price);
+		return;
+	}
+	return res.json({ didBid: false });
 });
 
-router.post("/lose", (req, res) => {
-    /*
-        adds to on memory campaign budget, takes price from redis
-     */
+router.post("/lose/:auction_id", (req, res) => {
+	const { auction_id } = req.params;
+	redisClient.hgetall(`auctions:${auction_id}`, (err, auction) => {
+		if (err) {
+			console.log("Error while fetching auction", err);
+			return res.status(500).json("Error while fetching auction");
+		}
+
+		console.log("auction", auction);
+
+		if (!auction) {
+			return res.status(404).json({ message: `auction ${auction_id} does not exist` });
+		}
+        campagins[auction.campaign_id] += +auction.price;
+		return res.status(200).json({});
+	});
 });
 
 module.exports = router;
-
